5f5309a009bf079eaf142f9b76183260632de455
Josh Ault <josh.ault3@gmail.com>
2016-04-28 19:09:15 -0500

    Initial Commit

---
+++ ChromeFixer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ChromeFixer
+//
+//  Created by Josh Ault on 4/28/16.
+//  Copyright © 2016 ault.io. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ChromeFixer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChromeFixer
+//
+//  Created by Josh Ault on 4/28/16.
+//  Copyright © 2016 ault.io. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
